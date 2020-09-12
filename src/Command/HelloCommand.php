af5b0aafa393c0a27c3b608d0d8dbce0d0bb031a
Kestutis Arlauskas <kestutisa@gmail.com>
2020-09-12 18:32:03 +0300

    soap client update

--- src/Command/HelloCommand.php
+++ src/Command/HelloCommand.php
@@ -3,25 +3,28 @@
 namespace App\Command;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 class HelloCommand extends Command
 {
-    protected static string $defaultName = 'app:hello';
+    protected static string $name = 'app:hello';
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
 
-        $soapClient = new \SoapClient('http://127.0.0.1:8000/soap?wsdl');
-        $result = $soapClient->call('hello', ['name' => 'Kes']);
+        $soapClient = new \SoapClient('localhost:8000/soap?wsdl');
+        $result = $soapClient->hello('hello', ['name' => 'Kes']);
 
         $io->success('Success.');
 
         return 0;
+    }
+
+    protected function configure()
+    {
+        $this->setName(self::$name);
     }
 }
